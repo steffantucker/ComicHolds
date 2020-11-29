96b387502976a2b5395ee6cf2f34ca9da2363f73
Steffan Tucker <40306798+steffantucker@users.noreply.github.com>
2020-11-28 21:01:16 -0700

    put and UUID changes (#2)
    configured PUT endpoint, changed IDs to UUIDs

--- data/comicsdata.go
+++ data/comicsdata.go
@@ -2,18 +2,24 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 // Comic struct for comic book info
 type Comic struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Issue       int    `json:"issue"`
-	TotalIssues int    `json:"total_issues"`
-	SeriesID    int    `json:"series_id"`
-	PublisherID int    `json:"publisher_id"`
+	ID          uuid.UUID `json:"id"`
+	Name        string    `json:"name"`
+	Issue       int       `json:"issue"`
+	TotalIssues int       `json:"total_issues"`
+	SeriesID    int       `json:"series_id"`
+	PublisherID int       `json:"publisher_id"`
 }
+
+// ComicNotFound is the error if the requested comic isn't found
+var ComicNotFound = fmt.Errorf("Comic not found")
 
 // Comics slice of multiple comics
 type Comics []*Comic
@@ -34,22 +40,37 @@ func GetComics() Comics {
 }
 
 // AddComic adds a comic to the database
-func AddComic(newComic *Comic) {
+func AddComic(newComic *Comic) error {
 	// TODO heck to make sure there's no conflict with existing
 	// comic. will need to check against name and also
 	// against issue number in series
-	newComic.ID = getNextID()
+	newComic.ID = uuid.NewV4()
 	comicsList = append(comicsList, newComic)
+	return nil
 }
 
-func getNextID() int {
-	lastID := comicsList[len(comicsList)-1].ID
-	return lastID + 1
+// UpdateComic updates the requested comic
+func UpdateComic(uid uuid.UUID, newData Comic) error {
+	id := getIndexfromProductID(uid)
+	if id == -1 {
+		return ComicNotFound
+	}
+	comicsList[id] = &newData
+	return nil
+}
+
+func getIndexfromProductID(id uuid.UUID) int {
+	for i, c := range comicsList {
+		if c.ID == id {
+			return i
+		}
+	}
+	return -1
 }
 
 var comicsList = Comics{
 	&Comic{
-		ID:          1,
+		ID:          uuid.NewV4(),
 		Name:        "Doom Patrol",
 		Issue:       1,
 		TotalIssues: 1,
